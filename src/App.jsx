8ef3df68c8344de32593376a3fc440a9faaf3860
a3l5o7n9 <39766138+a3l5o7n9@users.noreply.github.com>
2019-03-23 13:11:31 +0200

    Seperated Single Term Search and Multiple Terms Search into two different sections, each using its own customized search function.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
       images: [],
       searchTermsList: [],
       selectedSearchTerms: [],
-      selectedSearchType: "singleTerm",
+      selectedSearchType: "any",
       error: null
     }
   }
@@ -22,18 +22,6 @@ class App extends Component {
     }
     else {
       this.setState({ searchTermsList: [""] });
-    }
-  }
-
-  openSearchTermsList = () => {
-    if (this.state.searchTermsList !== undefined && this.state.searchTermsList !== []) {
-      return (
-        <div className="searchTermsSelect">
-          <select multiple={true} value={this.state.selectedSearchTerms} onChange={this.searchImages}>
-            {this.showSearchTermsList()}
-          </select>
-        </div>
-      );
     }
   }
 
@@ -51,85 +39,61 @@ class App extends Component {
     this.setState({ selectedSearchType });
   }
 
-  searchImages = (event) => {
+  singleSearchImages = (event) => {
     const searchTerm = event.target.value;
-    let selectedSearchTerms = this.state.selectedSearchTerms;
-    const selectedSearchType = this.state.selectedSearchType;
-    let tagString = "";
-    let tagModeString = "&tag_mode=any";
 
     if (searchTerm !== undefined && searchTerm !== "") {
       const cachedResult = localStorage.getItem(searchTerm);
       if (cachedResult) {
-        const searchTermImages = JSON.parse(cachedResult);
-
-        switch (selectedSearchType) {
-          case "singleTerm":
-            {
-              this.setState({ searchTerm, selectedSearchTerms: [searchTerm], images: searchTermImages });
-              return;
-            }
-          default:
-            {
-              break;
-            }
-        }
+        this.setState({ searchTerm, images: JSON.parse(cachedResult) });
+        return;
       }
 
-      if (selectedSearchType === "multipleTermAND" || selectedSearchType === "multipleTermOR") {
-        if (selectedSearchTerms.includes(searchTerm)) {
-          selectedSearchTerms.splice(selectedSearchTerms.indexOf(searchTerm), 1);
-        }
-        else {
-          selectedSearchTerms.push(searchTerm);
-        }
+      const searchTermsList = [...this.state.searchTermsList];
 
-        tagString = "&tags=" + selectedSearchTerms[0];
+      searchTermsList.push(searchTerm);
+      localStorage.setItem("searchTermsList", JSON.stringify(searchTermsList));
 
-        for (var i = 1, l = selectedSearchTerms.length; i < l; i++) {
-          tagString += "%2C" + selectedSearchTerms[i];
-        }
+      fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=bac9f1ccfd854f27894fd47c4f01b1e8&content_type=1&is_getty=1&per_page=100&text=" + searchTerm)
+        .then(res => res.json())
+        .then(
+          (result) => {
+            localStorage.setItem(searchTerm, JSON.stringify(result.photos.photo));
+            this.setState({ searchTerm, images: result.photos.photo, searchTermsList });
+          },
+          (error) => {
+            console.log(error);
+            this.setState({ error });
+          }
+        )
+      return;
+    }
+    else {
+      this.setState({ searchTerm: "", images: [] });
+    }
+  }
+
+  multipleSearchImages = (event) => {
+    const selectedSearchTerm = event.target.value;
+    let selectedSearchTerms = this.state.selectedSearchTerms;
+    const selectedSearchType = this.state.selectedSearchType;
+    let tagString = "";
+
+    if (selectedSearchTerm !== undefined && selectedSearchTerm !== "") {
+      if (selectedSearchTerms.includes(selectedSearchTerm)) {
+        selectedSearchTerms.splice(selectedSearchTerms.indexOf(selectedSearchTerm), 1);
+      }
+      else {
+        selectedSearchTerms.push(selectedSearchTerm);
       }
 
-      switch (selectedSearchType) {
-        case "singleTerm":
-          {
-            const searchTermsList = [...this.state.searchTermsList];
+      tagString = selectedSearchTerms[0];
 
-            searchTermsList.push(searchTerm);
-            localStorage.setItem("searchTermsList", JSON.stringify(searchTermsList));
-
-            fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=bac9f1ccfd854f27894fd47c4f01b1e8&content_type=1&is_getty=1&per_page=100&text=" + searchTerm)
-              .then(res => res.json())
-              .then(
-                (result) => {
-                  localStorage.setItem(searchTerm, JSON.stringify(result.photos.photo));
-                  this.setState({ searchTerm, selectedSearchTerms, images: result.photos.photo, searchTermsList });
-                },
-                (error) => {
-                  console.log(error);
-                  this.setState({ error });
-                }
-              )
-            return;
-          }
-        case "multipleTermAND":
-          {
-            tagModeString = "&tag_mode=all";
-            break;
-          }
-        case "multipleTermOR":
-          {
-            tagModeString = "&tag_mode=any";
-            break;
-          }
-        default:
-          {
-            break;
-          }
+      for (var i = 1, l = selectedSearchTerms.length; i < l; i++) {
+        tagString += "%2C" + selectedSearchTerms[i];
       }
 
-      fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=bac9f1ccfd854f27894fd47c4f01b1e8&content_type=1&is_getty=1&per_page=100" + tagString + tagModeString)
+      fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&safe_search=1&format=json&nojsoncallback=1&api_key=bac9f1ccfd854f27894fd47c4f01b1e8&content_type=1&is_getty=1&per_page=100&tags=" + tagString + "&tag_mode=" + selectedSearchType)
         .then(res => res.json())
         .then(
           (result) => {
@@ -141,8 +105,8 @@ class App extends Component {
           }
         )
     }
-    else {
-      this.setState({ searchTerm: "", selectedSearchTerms: [""], images: [] });
+    else if (selectedSearchTerms === undefined || selectedSearchTerms === []) {
+      this.setState({ selectedSearchTerms: [""], images: [] });
     }
   }
 
@@ -165,22 +129,36 @@ class App extends Component {
         <div className="AppHeader">
           <h1>Image Gallery</h1>
           <form>
-            <input
-              type="text"
-              name="searchTerm"
-              value={this.state.searchTerm}
-              placeholder="search for images...."
-              onChange={this.searchImages}
-            />
-            <div className="searchTypes">
-              <select value={this.state.selectedSearchType} onChange={this.changeSearchType}>
-                <option value="singleTerm">Single Term</option>
-                <option value="multipleTermAND">Multiple - AND</option>
-                <option value="multipleTermOR">Multiple - OR</option>
-              </select>
+            <div className="SingleTermSearch">
+              <h3>Single Term Search</h3>
+              <input
+                type="text"
+                name="searchTerm"
+                value={this.state.searchTerm}
+                placeholder="search for images...."
+                onChange={this.singleSearchImages}
+              />
+              <div className="SingleSearchTermSelect">
+                <select value={this.state.searchTerm} onChange={this.singleSearchImages}>
+                  {this.showSearchTermsList()}
+                </select>
+              </div>
             </div>
-            <div className="selectList">
-              {this.openSearchTermsList()}
+            <div className="MultipleTermsSearch">
+              <h3>Multiple Terms Search</h3>
+              <div className="searchTypes">
+                <select value={this.state.selectedSearchType} onChange={this.changeSearchType}>
+                  <option value="all">AND</option>
+                  <option value="any">OR</option>
+                </select>
+              </div>
+              <div className="selectList">
+                <div className="MultipleSearchTermsSelect">
+                  <select multiple={true} value={this.state.selectedSearchTerms} onChange={this.multipleSearchImages}>
+                    {this.showSearchTermsList()}
+                  </select>
+                </div>
+              </div>
             </div>
           </form>
         </div>
